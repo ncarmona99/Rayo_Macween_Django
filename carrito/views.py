d659198f769011317fa95c27e90407584ec1497d
visolorza <vi.solorza@duocuc.cl>
2024-07-03 17:45:11 -0400

    Se termina el carro de compras con funcionalidad

--- carrito/views.py
+++ carrito/views.py
@@ -3,52 +3,34 @@ from .carrito import Carrito
 from RayoMacween.models import Producto
 from django.http import JsonResponse
 
+
 def carrito_resumen(request):
-	return render(request, "carrito_resumen.html", {})
+    carrito = Carrito(request)
+    carrito_productos = carrito.obtener_productos()
+    return render(request, "carrito_resumen.html", {"carrito_productos": carrito_productos})
 
 def carrito_add(request):
-	
-	carrito = Carrito(request)
-	if request.POST.get('action') == 'post':
-		producto_id = int(request.POST.get('producto_id'))
-		#producto_qty = int(request.POST.get('product_qty'))
-
-		producto = get_object_or_404(Producto, id=producto_id)
-
-		carrito.add(producto=producto)
-
-		#carrito_quantity = carrito.__len__()
-
-		response = JsonResponse({'Nombre producto': producto.nombre})
-		#messages.success(request, ("Producto añadido al carrito"))
-		return response
-
-def carrito_delete(request):
-	pass
-	'''cart = Cart(request)
-	if request.POST.get('action') == 'post':
-		# Get stuff
-		product_id = int(request.POST.get('product_id'))
-		# Call delete Function in Cart
-		cart.delete(product=product_id)
-
-		response = JsonResponse({'product':product_id})
-		#return redirect('cart_summary')
-		messages.success(request, ("Item Deleted From Shopping Cart..."))
-		return response'''
-
+    carrito = Carrito(request)
+    producto_id = int(request.POST.get('producto_id'))
+    producto = get_object_or_404(Producto, id=producto_id)
+    cantidad = int(request.POST.get('cantidad', 1))
+    carrito.add(producto=producto, cantidad=cantidad)
+    return JsonResponse({'total_cantidad': carrito.__len__()})
 
 def carrito_update(request):
-	pass
-	'''cart = Cart(request)
-	if request.POST.get('action') == 'post':
-		# Get stuff
-		product_id = int(request.POST.get('product_id'))
-		product_qty = int(request.POST.get('product_qty'))
+    carrito = Carrito(request)
+    producto_id = int(request.POST.get('producto_id'))
+    cantidad = int(request.POST.get('cantidad'))
+    producto = get_object_or_404(Producto, id=producto_id)
+    if cantidad > 0:
+        carrito.update(producto=producto, cantidad=cantidad)
+    else:
+        carrito.delete(producto=producto)
+    return JsonResponse({'total_cantidad': carrito.__len__(), 'total_price': carrito.obtener_precio_total()})
 
-		cart.update(product=product_id, quantity=product_qty)
-
-		response = JsonResponse({'qty':product_qty})
-		#return redirect('cart_summary')
-		messages.success(request, ("Your Cart Has Been Updated..."))
-		return response'''
+def carrito_delete(request):
+    carrito = Carrito(request)
+    producto_id = int(request.POST.get('producto_id'))
+    producto = get_object_or_404(Producto, id=producto_id)
+    carrito.delete(producto=producto)
+    return JsonResponse({'total_cantidad': carrito.__len__(), 'total_price': carrito.obtener_precio_total()})
